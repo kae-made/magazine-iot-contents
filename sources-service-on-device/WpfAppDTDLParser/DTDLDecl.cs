b0923b66dc54a595908fd376541ad656a576ea64
kae-made <93808496+kae-made@users.noreply.github.com>
2022-10-06 08:52:00 +0900

    Add dtdl parser app

---
+++ sources-service-on-device/WpfAppDTDLParser/DTDLDecl.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Azure.DigitalTwins.Parser;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDTDLParser
+{
+    public class DTDLDecl
+    {
+        private List<string> extends = new List<string>();
+        private List<string> properties = new List<string>();
+        private List<string> telemetries = new List<string>();
+        private List<string> commands = new List<string>();
+        private List<(string name, string target)> relationships = new List<(string name, string target)>();
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public List<string> Extends { get { return extends; } }
+        public List<string> Properties { get { return properties; } }
+        public List<string> Telemetries { get { return telemetries; } }
+        public List<string> Commands { get { return commands; } }
+        public List<(string name, string target)> Relationships { get { return relationships; } }
+
+        public void Resolve(DTInterfaceInfo dtdlDecl)
+        {
+            Id = dtdlDecl.Id.AbsolutePath;
+            foreach (var ifName in dtdlDecl.DisplayName)
+            {
+                Name = ifName.Value;
+                break;
+            }
+            foreach(var ext in dtdlDecl.Extends)
+            {
+                extends.Add(ext.Id.AbsolutePath);
+            }
+            foreach( var contentKey in dtdlDecl.Contents.Keys)
+            {
+                var contentDecl = dtdlDecl.Contents[contentKey];
+                switch (contentDecl.EntityKind)
+                {
+                    case DTEntityKind.Property:
+                        var propDecl = (DTPropertyInfo)contentDecl;
+                        properties.Add(propDecl.Name);
+                        break;
+                    case DTEntityKind.Relationship:
+                        var relDecl = (DTRelationshipInfo)contentDecl;
+                        relationships.Add((name:relDecl.Id.AbsolutePath,target:relDecl.Target.AbsolutePath));
+                        break;
+                    case DTEntityKind.Telemetry:
+                        var telmDecl = (DTTelemetryInfo)contentDecl;
+                        var telemetryDecl = (DTTelemetryInfo)contentDecl;
+                        telemetries.Add(telemetryDecl.Name);
+                        break;
+                    case DTEntityKind.Command:
+                        var commandDecl = (DTCommandInfo)contentDecl;
+                        commands.Add(commandDecl.Name);
+                        break;
+                }
+            }
+        }
+
+        public TreeViewData GetTreeViewData()
+        {
+            var result = new TreeViewData() { Name = Id, Children = new ObservableCollection<TreeViewData>() };
+            if (extends.Count > 0)
+            {
+
+                var extendsTVD = new TreeViewData() { Name="extends", Children = new ObservableCollection<TreeViewData>() };
+                foreach(var ext in extends)
+                {
+                    extendsTVD.Children.Add(new TreeViewData() { Name = ext });
+                }
+                result.Children.Add(extendsTVD);
+            }
+            if (properties.Count > 0)
+            {
+                var propsTVD = new TreeViewData() { Name = "properties", Children = new ObservableCollection<TreeViewData>() };
+                foreach(var prop in properties)
+                {
+                    propsTVD.Children.Add(new TreeViewData() { Name = prop });
+                }
+            }
+            if (relationships.Count> 0)
+            {
+                var relsTVD = new TreeViewData() { Name = "relationships", Children = new ObservableCollection<TreeViewData>() };
+                foreach(var rel in relationships)
+                {
+                    relsTVD.Children.Add(new TreeViewData() { Name = $"{rel.name} -> {rel.target}" });
+                }
+            }
+            if (telemetries.Count > 0)
+            {
+                var telsTVD = new TreeViewData() { Name="telemetries", Children= new ObservableCollection<TreeViewData>() };
+                foreach(var tel in telemetries)
+                {
+                    telsTVD.Children.Add(new TreeViewData() { Name = tel, Children = new ObservableCollection<TreeViewData>() });
+                }
+            }
+            if (commands.Count > 0)
+            {
+                var cmdsTVD = new TreeViewData() { Name = "commands", Children = new ObservableCollection<TreeViewData>() };
+                foreach(var cmd in commands)
+                {
+                    cmdsTVD.Children.Add(new TreeViewData() { Name = cmd, Children = new ObservableCollection<TreeViewData>() });
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ sources-service-on-device/WpfAppDTDLParser/MainWindow.xaml.cs
@@ -0,0 +1,236 @@
+﻿using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using Kae.Utility.Logging;
+using Microsoft.Azure.DigitalTwins.Parser;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDTDLParser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<DTDLDecl> dtdlDecls = new List<DTDLDecl>();
+        private Dictionary<string, string> dtdlFiles = new Dictionary<string, string>();
+        ObservableCollection<TreeViewData> parsedDTDLs = new ObservableCollection<TreeViewData>();
+        private Logger logger;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            tvParsedDTDL.ItemsSource = parsedDTDLs;
+            logger = new TextBlockLogger(this.tbLog);
+        }
+
+        private void buttonSelectDTDLFiles_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "DTDL Files(.json)|*.json";
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog() == true)
+            {
+                dtdlFiles.Clear();
+                tbDTDLFiles.Text = "";
+                buttonParseDTDLFiles.IsEnabled = false;
+                foreach (var file in dialog.FileNames)
+                {
+                    var fileInfo = new FileInfo(file);
+                    if (!string.IsNullOrEmpty(tbDTDLFiles.Text))
+                    {
+                        tbDTDLFiles.Text += ", ";
+                    }
+                    tbDTDLFiles.Text += fileInfo.Name;
+                    dtdlFiles.Add(fileInfo.Name ,file);
+                }
+            }
+            if (dtdlFiles.Count > 0)
+            {
+                buttonParseDTDLFiles.IsEnabled = true;
+            }
+        }
+
+        List<string> modelsJson = new List<string>();
+
+        private async void buttonParseDTDLFiles_Click(object sender, RoutedEventArgs e)
+        {
+            var parser = new ModelParser();
+            try
+            {
+                modelsJson.Clear();
+                foreach (var dtdlFile in dtdlFiles.Keys)
+                {
+                    using (var reader = new StreamReader(dtdlFiles[dtdlFile]))
+                    {
+                        var json = reader.ReadToEnd();
+                        string dtdlFileName = dtdlFile.Substring(0, dtdlFile.LastIndexOf(".json"));
+                        if (dtdlFileName.LastIndexOf("_iotpnp") < 0)
+                        {
+                            modelsJson.Add(json);
+                        }
+                        await logger.LogInfo($"Loaded  - {dtdlFile}");
+                        var parseResultForJson = await parser.ParseAsync(new List<string>() { json });
+                        await logger.LogInfo($"Checked - {dtdlFile}");
+                        foreach(var declKey in parseResultForJson.Keys)
+                        {
+                            var declFrag = parseResultForJson[declKey];
+                            if (declFrag != null)
+                            {
+                                switch (declFrag.EntityKind)
+                                {
+                                    case DTEntityKind.Interface:
+                                        var declInterface = (DTInterfaceInfo)declFrag;
+                                        var declIf = new DTDLDecl();
+                                        declIf.Resolve(declInterface);
+                                        parsedDTDLs.Add(declIf.GetTreeViewData());
+                                        break;
+                                    case DTEntityKind.Component:
+                                        break;
+                                    case DTEntityKind.Command:
+                                        break;
+                                    case DTEntityKind.CommandPayload:
+                                        break;
+                                    case DTEntityKind.CommandType:
+                                        break;
+                                    case DTEntityKind.String:
+                                        break;
+                                    case DTEntityKind.Integer:
+                                        break;
+                                    case DTEntityKind.Array:
+                                        break;
+                                    case DTEntityKind.Boolean:
+                                        break;
+                                    case DTEntityKind.Date:
+                                        break;
+                                    case DTEntityKind.DateTime:
+                                        break;
+                                    case DTEntityKind.Double:
+                                        break;
+                                    case DTEntityKind.Duration:
+                                        break;
+                                    case DTEntityKind.Long:
+                                        break;
+                                    case DTEntityKind.Enum:
+                                        break;
+                                    case DTEntityKind.EnumValue:
+                                        break;
+                                    case DTEntityKind.Object:
+                                        break;
+                                    case DTEntityKind.Field:
+                                        break;
+                                    case DTEntityKind.Float:
+                                        break;
+                                    case DTEntityKind.Map:
+                                        break;
+                                    case DTEntityKind.MapKey:
+                                        break;
+                                    case DTEntityKind.MapValue:
+                                        break;
+                                    case DTEntityKind.Property:
+                                        break;
+                                    case DTEntityKind.Reference:
+                                        break;
+                                    case DTEntityKind.Relationship:
+                                        break;
+                                    case DTEntityKind.Telemetry:
+                                        break;
+                                    case DTEntityKind.Time:
+                                        break;
+                                    case DTEntityKind.Unit:
+                                        break;
+                                    case DTEntityKind.UnitAttribute:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+                buttonUploadDTDL.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    await logger.LogError(GetInnerExeptionMessage(ex.InnerException));
+                }
+                await logger.LogError(ex.Message);
+            }
+        }
+
+        private string GetInnerExeptionMessage(Exception ex)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(ex.Message);
+            if (ex.InnerException != null)
+            {
+                sb.Append(GetInnerExeptionMessage(ex.InnerException));
+            }
+            return sb.ToString();
+        }
+
+        private async void buttonUploadDTDL_Click(object sender, RoutedEventArgs e)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            string adtInstanceUrl = configuration.GetConnectionString("ADT"); 
+
+            var credential = new DefaultAzureCredential();
+            try
+            {
+                var client = new DigitalTwinsClient(new Uri(adtInstanceUrl), credential);
+                await logger.LogInfo($"Service client created – ready to go");
+                await client.CreateModelsAsync(modelsJson);
+            }
+            catch (Exception ex)
+            {
+                await logger.LogError($"{ex.Message}");
+            }
+        }
+
+    }
+    public class TreeViewData
+    {
+        public string Name { get; set; }
+        public ObservableCollection<TreeViewData> Children { get; set; }
+    }
+
+    class TextBlockLogger : Logger
+    {
+        private TextBlock textBlock;
+        public TextBlockLogger(TextBlock tb)
+        {
+            textBlock = tb;
+        }
+
+        protected override async Task LogInternal(Level level, string log, string timestamp)
+        {
+            textBlock.Dispatcher.Invoke(() =>
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine($"{timestamp}:[{level.ToString()}] {log}");
+                sb.Append(textBlock.Text);
+                textBlock.Text = sb.ToString();
+            });
+        }
+    }
+}

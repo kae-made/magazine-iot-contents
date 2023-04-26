16d51b937d82b19a4a9c81a67466b83bc9f6c46d
kae-made <93808496+kae-made@users.noreply.github.com>
2023-04-26 09:13:05 +0900

    Add RTSP to File on Windows

---
+++ file-uploads/WpfAppRTSP2Images/WpfAppRTSP2Images/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppRTSP2Images
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        RTSP2Images rtsp2images = null;
+
+        private async void buttonControl_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonControl.Content.ToString() == "Start")
+            {
+                if (rtsp2images != null)
+                {
+                    await rtsp2images.StopAsync(ShowLog);
+                }
+                try
+                {
+                    rtsp2images = new RTSP2Images(tbURL.Text, ((ComboBoxItem)cbFormat.SelectedItem).Content.ToString(), int.Parse(tbFPS.Text),
+                       async (w) => { tbWidth.Text = $"{w}"; }, async (h) => { tbHeight.Text = $"{h}"; }, tbFolder.Text);
+                    await rtsp2images.StartAsync(ShowLog);
+                    buttonControl.Content = "Stop";
+                }
+                catch (Exception ex) { Debug.WriteLine(ex.Message); }
+            }
+            else
+            {
+                if (rtsp2images != null)
+                {
+                    await rtsp2images.StopAsync(ShowLog);
+                    rtsp2images = null;
+                }
+            }
+        }
+
+        private async Task ShowLog(string message)
+        {
+            var sb = new StringBuilder(tbLog.Text);
+            using (var writer = new StringWriter(sb))
+            {
+                writer.WriteLine(message);
+            }
+            tbLog.Text = sb.ToString();
+        }
+    }
+}
---
+++ file-uploads/WpfAppRTSP2Images/WpfAppRTSP2Images/RTSP2Images.cs
@@ -0,0 +1,93 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfAppRTSP2Images
+{
+    public delegate Task ShowLog(string message);
+    public delegate Task SetWidth(int  width);
+    public delegate Task SetHeight(int height);
+    public class RTSP2Images
+    {
+        private string RTSPUrl;
+        private int OutputWidth;
+        private int OutputHeight;
+        private string OutputFormat;
+        private int CaptureFPS;
+        private string OutputPath;
+
+        private SetWidth setWidth;
+        private SetHeight setHeight;
+
+        private VideoCapture videoCapture;
+        public RTSP2Images(string url, string format, int fps, SetWidth widthSet, SetHeight heightSet, string outputPath)
+        {
+            RTSPUrl = url;
+            OutputFormat = format;
+            CaptureFPS = fps;
+
+            setWidth = widthSet;
+            setHeight = heightSet;
+
+            videoCapture = new VideoCapture(RTSPUrl);
+            
+            OutputPath = outputPath;
+        }
+
+        private CancellationTokenSource cts;
+
+        public async Task StartAsync(ShowLog showLog)
+        {
+            var tmp = new Mat(1024, 768, MatType.CV_8UC3);
+            //if (!videoCapture.Open(RTSPUrl))
+            if (!videoCapture.IsOpened())
+            {
+                await showLog($"RTSP stream has not been opened.");
+                return;
+            }
+            if (!videoCapture.Grab())
+            {
+                await showLog($"RTSP stream has not been grabed.");
+                return;
+            }
+            await showLog($"Starting at {DateTime.Now.ToString("yyyy/MM/dd-HH:mm:ss")}");
+
+            //OutputHeight = (videoCapture.FrameHeight * OutputWidth) / videoCapture.FrameWidth;
+            //videoCapture.FrameWidth = OutputWidth;
+            //videoCapture.FrameHeight = OutputHeight;
+            setWidth(videoCapture.FrameWidth);
+            setHeight(videoCapture.FrameHeight);
+
+            cts = new CancellationTokenSource();
+
+            while (true)
+            {
+                var output = new Mat(videoCapture.FrameHeight, videoCapture.FrameWidth, MatType.CV_8UC3);
+                if (videoCapture.Read(output))
+                {
+                    string filename = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.{OutputFormat}";
+                    string filepath = Path.Join(OutputPath, filename);
+                    output.SaveImage(filepath);
+                    await showLog($"Saved - {filename}");
+                }
+                if (cts.Token.IsCancellationRequested)
+                {
+                    cts.Token.ThrowIfCancellationRequested();
+                }
+                await Task.Delay(TimeSpan.FromSeconds(CaptureFPS),cts.Token);
+            }
+        }
+
+        public async Task StopAsync(ShowLog showLog)
+        {
+            await showLog($"Stopped at {DateTime.Now.ToString("yyyy/MM/dd-HH:mm:ss")}");
+            cts.Cancel();
+            cts.Dispose();
+        }
+    }
+}
